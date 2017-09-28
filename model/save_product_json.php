538bb12802ba1dccc5c8fd312e5ff3a4f2e98d1d
pixelerator <mohammed_intekhab@hotmail.com>
2017-09-29 01:29:09 +0530

    Added database library/validation library

--- model/save_product_json.php
+++ model/save_product_json.php
@@ -1,6 +1,9 @@
 <?php
+
+require_once "../vendor/autoload.php";
 $params = json_decode(file_get_contents('php://input'),true);
 
+$db = new MysqliDb ('localhost', 'root', '', 'meshproduct');
 
 echo "<pre>";
     print_r($params);
